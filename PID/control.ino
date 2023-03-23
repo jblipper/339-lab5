8eb7fceba5e18ad9efc2e4d8cd8bcb851ec83deb
Bhuvan <bhuvanmanojh@gmail.com>
2023-03-23 10:54:17 -0400

    arduino code

--- PID/control.ino
+++ PID/control.ino
@@ -4,6 +4,8 @@ boolean _debug = true;
 float u1;  // First  user debug variable
 float u2;  // Second user debug variable
 float u3;  // Third  user debug variable
+float E;
+float I;
 
 void control(){
   /*
@@ -16,11 +18,17 @@ void control(){
     
    float error;       // Temperature error relative to setpoint 
    error = temperature - setpoint; 
-  
+   
   if (error >= band/2) {
     set_dac(-4095);
   } 
   else if (error < -1*band/2) {
     set_dac(0);
   }
+ else {
+  E = -error/band;
+  I+=(1/t_integral)*(E*(float(dt)/10000))
+  
+  set_dac((2*E+I)*4095);
+}
 }
